7c47b70842523cf0c6a1d8d7bb5caf624d5cfc4e
Ghravitee <dyenamics228@gmail.com>
2024-05-17 02:06:18 +0100

    fix about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,24 +7,27 @@ import sponge3 from "../../public/sponge3.png"
 const About = () => {
   return (
     <>
-    <div className="flex flex-col lg:flex-row justify-center gap-10 mt-20 md:mt-0">
+    <div className="flex flex-col md:flex-row justify-center gap-10 mt-20 md:mt-0">
         <Image
         src="/sponge1.PNG"
         alt='sponge1'
         width={250}
         height={250}
+        className='mx-auto'
         />
         <Image
         src="/sponge2.PNG"
         alt='sponge1'
         width={250}
         height={250}
+        className='mx-auto'
         />
         <Image
         src="/sponge3.PNG"
         alt='sponge1'
         width={250}
         height={250}
+        className='mx-auto'
         />
     </div>
     
